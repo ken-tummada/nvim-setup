f829d82d0d0e4e4a3acf9205b2b42fe59f64cf0b
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2025-11-11 03:41:02 -0800

    add: setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!usr/bin/sh
+
+mkdir "$HOME/tmp"
+
+curl -L -o "$HOME/tmp/nvim.tar.gz"\ 
+    https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+mkdir -p "$HOME/tmp/.local"
+
+tar -xzf "$HOME/tmp/nvim.tar.gz" -C "$HOME/.local" --strip-components=1
+
+export PATH="$HOME/.local/bin:$PATH"
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.profile"
+
+rm "$HOME/tmp/nvim.tar.gz"
+
+mkdir -p "$HOME/.config/nvim"
+
+ln -s ./init.lua "$HOME/.config/nvim/init.lua"
